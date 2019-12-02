1691e5bfa07f12dac34d4409079ed8d20254a997
David Boedigheimer <stealth259@gmail.com>
2019-12-02 15:28:04 -0800

    Add third pass.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 // Back end
-function Location(placeName, landmarks, year, favoriteRestaurant, nextTime) {
+function Location(placeName) {//landmarks, year, favoriteRestaurant, nextTime) {
   this.placeName = placeName;
-  this.landmarks = landmarks;
-  this.year = year;
-  this.favoriteRestaurant = favoriteRestaurant;
-  this.nextTime = nextTime;
-  console.log(placeName, landmarks, year, favoriteRestaurant, nextTime);
+  // this.landmarks = landmarks;
+  // this.year = year;
+  // this.favoriteRestaurant = favoriteRestaurant;
+  // this.nextTime = nextTime;
+  // console.log(placeName, landmarks, year, favoriteRestaurant, nextTime);
 }
 
 // Front end
+$(document).ready(function() {
+  $("form#addPlace").submit(function(e) {
+    event.preventDefault();
+    var placeName = $("#place").val();
+    var newLocation = new Location(placeName);
+    console.log(newLocation);
+  })
+})
