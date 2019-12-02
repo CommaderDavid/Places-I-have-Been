8c474ea88c97362448afdbb47b3f5e8f104216c4
David Boedigheimer <stealth259@gmail.com>
2019-12-02 15:51:25 -0800

    edit on location function and adding more to form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Back end
-function Location(placeName) {//landmarks, year, favoriteRestaurant, nextTime) {
+function Location(placeName, landmark, year, favoriteRestaurant, nextTime) {
   this.placeName = placeName;
-  // this.landmarks = landmarks;
-  // this.year = year;
+  this.landmark = landmark;
+  this.year = year;
   // this.favoriteRestaurant = favoriteRestaurant;
   // this.nextTime = nextTime;
   // console.log(placeName, landmarks, year, favoriteRestaurant, nextTime);
